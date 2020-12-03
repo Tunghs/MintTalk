94173d65df15d2ef4c58db24f13672fce6177149
이성현 <juk08843@gmail.com>
2020-12-04 00:19:54 +0900

    my sql 참조 추가

---
+++ MintTalk/LoginWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace MintTalk
+{
+    /// <summary>
+    /// LoginWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class LoginWindow : MetroWindow
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
